3118232de31a426688e6239dd7184ba1392f3e6d
TOEN-A <viewkanafurefuyu@gmail.com>
2023-08-14 23:30:09 +0900

    Android版選択ボタンのレイアウト崩れ修正

--- app/cards/components/selectButton.tsx
+++ app/cards/components/selectButton.tsx
@@ -16,7 +16,7 @@ const SelectButton: React.FC<{
       {options.map((option) => (
         <button
           key={option}
-          className={"mr-0.5 mt-0.5 text-[0.68rem] md:text-sm lg:text-base w-[3.4rem] md:w-16 lg:w-24 px-0 py-2 rounded-full focus:outline-none hover:opacity-75 shadow-md"}
+          className={"mr-0.5 mt-0.5 text-[0.64rem] md:text-sm lg:text-base w-[3.4rem] md:w-16 lg:w-24 px-0 py-2 rounded-full focus:outline-none hover:opacity-75 shadow-md"}
           style={{
             background: selectedOptions.includes(option) ? colors[option] : 'rgba(128, 128, 128, 0.7)',
             color: selectedOptions.includes(option) ? 'white' : 'black',
