72727ff7b7d2ac7a60610a9e22c7ca47a1a69fa0
TOEN-A <viewkanafurefuyu@gmail.com>
2023-10-29 00:50:08 +0900

    WebGL部分の軽量化

--- app/canvas/Ball.tsx
+++ app/canvas/Ball.tsx
@@ -19,9 +19,14 @@ const Ball = ({
   scale: number
   selected: string
 }) => {
-  const [map] = useTexture([url || '']);
+  const [map] = useTexture([url || ''])
 
   const selectedClasses = useContext(SelectedOption)
+  const materialColor = useMemo(
+    () => (selectedClasses.includes(selected) ? '#66CDAA' : '#fff8eb'),
+    [selectedClasses, selected]
+  )
+
   return (
     <>
       <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
@@ -30,16 +35,16 @@ const Ball = ({
         <mesh castShadow receiveShadow scale={scale}>
           <icosahedronGeometry args={[1, 1]} />
           <meshStandardMaterial
-            color={selectedClasses.includes(selected) ? '#66CDAA' : '#fff8eb'}
+            color={materialColor}
             polygonOffset
             polygonOffsetFactor={-5}
             flatShading
           />
           <Decal
-              position={[0, 0, 1]}
-              rotation={[2 * Math.PI, 0, 6.25]}
-              map={map}
-            />
+            position={[0, 0, 1]}
+            rotation={[2 * Math.PI, 0, 6.25]}
+            map={map}
+          />
         </mesh>
       </Float>
     </>
